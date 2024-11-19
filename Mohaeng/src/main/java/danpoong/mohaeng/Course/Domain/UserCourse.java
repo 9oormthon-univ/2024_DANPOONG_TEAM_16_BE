9c2df4b526e4c084649ca09f9e97496d0daf2df5
genmu5 <genmu4@skuniv.ac.kr>
2024-11-20 01:07:49 +0900

    feat : 관광지 지역 및 유저 코스 도메인 정리 및 JPA 레포지토리 추가 추후 코스정보 추가 예정

---
+++ Mohaeng/src/main/java/danpoong/mohaeng/Course/Domain/UserCourse.java
@@ -0,0 +1,32 @@
+package danpoong.mohaeng.Course.Domain;
+
+
+import danpoong.mohaeng.Location.Domain.Location;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_course")
+@NoArgsConstructor
+@Getter
+public class UserCourse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_course_number")
+    private Long userCourseNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "course_number")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "content_id")
+    private Location location;
+
+    public UserCourse(Course course, Location location) {
+        this.course = course;
+        this.location = location;
+    }
+}

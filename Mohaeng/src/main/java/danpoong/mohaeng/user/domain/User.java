b62d8aded6424b9a2d9f551d7abadf9021ea3481
KSJ0128 <ksj01128@naver.com>
2024-11-20 14:03:04 +0900

    refactor: 도메인 수정 및 생성
    #4 이슈에서 정의한 도메인을 생성 및 수정했습니다.

---
+++ Mohaeng/src/main/java/danpoong/mohaeng/user/domain/User.java
@@ -0,0 +1,22 @@
+package danpoong.mohaeng.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @Column(name = "uuid", nullable = false)
+    private String uuid;
+}

e0738dd4d3daed35330fcc520bbc84e6064b66d2
KSJ0128 <ksj01128@naver.com>
2024-11-21 20:51:09 +0900

    feat: @Schema 설명 추가
    UserCreateReq에 @Schema를 추가해 Swagger에서 해당 DTO에 대한 설명을 추가했습니다.

--- Mohaeng/src/main/java/danpoong/mohaeng/user/dto/UserCreateReq.java
+++ Mohaeng/src/main/java/danpoong/mohaeng/user/dto/UserCreateReq.java
@@ -1,8 +1,10 @@
 package danpoong.mohaeng.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "uuid = 유저 고유 ID")
 public class UserCreateReq {
     private String uuid;
 }

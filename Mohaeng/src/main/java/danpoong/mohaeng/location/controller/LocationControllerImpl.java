2ef6a913f316a5070487a873a0efaf7a7c719eaf
KSJ0128 <ksj01128@naver.com>
2024-11-22 13:20:33 +0900

    feat: Swagger 적용 - 관광지 상세 정보 조회
    Swagger를 통한 API 명세서 작성을 위한 코드를 구현했습니다.
    LocationController 인터페이스를 통해 Swagger 어노테이션을 분리했습니다.
    
    추가로 UserControllerImpl의 어노테이션을 수정해주었습니다.

--- Mohaeng/src/main/java/danpoong/mohaeng/location/controller/LocationControllerImpl.java
+++ Mohaeng/src/main/java/danpoong/mohaeng/location/controller/LocationControllerImpl.java
@@ -2,17 +2,19 @@ package danpoong.mohaeng.location.controller;
 
 import danpoong.mohaeng.location.dto.LocationInfoRes;
 import danpoong.mohaeng.location.service.LocationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/location")
 @RequiredArgsConstructor
+@Tag(name = "Location", description = "관광지 관련 API")
 public class LocationControllerImpl implements LocationController {
 
     private final LocationService locationService;

32aaff0aab5bb8e6b9c2b9e1e4510a96c8e8ff2e
KSJ0128 <ksj01128@naver.com>
2024-11-22 22:47:07 +0900

    feat: AI 추천 코스 제공 로직 구현
    generateCourse()에서 필터링 된 관광지 및 음식점과 프롬프트를 생성해 파인튜닝한 모델에 여행 코스를 요청합니다.
    AI가 리스트를 기반으로 추천한 여행 코스를 받아와 Response에 맞게 contentId를 파싱하고, 리턴합니다.
    
    추후, DB 인덱스 생성, 프롬프트 최적화를 통한 성능 향상이 필요합니다.

--- Mohaeng/src/main/java/danpoong/mohaeng/course/controller/CourseControllerImpl.java
+++ Mohaeng/src/main/java/danpoong/mohaeng/course/controller/CourseControllerImpl.java
@@ -1,15 +1,15 @@
 package danpoong.mohaeng.course.controller;
 
+import danpoong.mohaeng.course.dto.AICourseRes;
 import danpoong.mohaeng.course.dto.CourseCreateReq;
 import danpoong.mohaeng.course.dto.CourseCreateRes;
 import danpoong.mohaeng.course.service.CourseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/course")
@@ -26,5 +26,16 @@ public class CourseControllerImpl implements CourseController{
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(courseCreateRes);
+    }
+
+    @GetMapping("/ai-rec")
+    public ResponseEntity<AICourseRes> courseCreate(@RequestParam("diability") List<Long> disability,
+            @RequestParam("tripType") List<Long> tripType, @RequestParam("area") Long area, @RequestParam("period") Long period) {
+        AICourseRes aiCourseRes = courseService.createAIRecCourse(disability, tripType, area, period);
+
+        if (aiCourseRes == null || aiCourseRes.getDay1() == null)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(aiCourseRes);
     }
 }
--- Mohaeng/src/main/java/danpoong/mohaeng/course/service/CourseService.java
+++ Mohaeng/src/main/java/danpoong/mohaeng/course/service/CourseService.java
@@ -4,6 +4,7 @@ import danpoong.mohaeng.area.domain.Area;
 import danpoong.mohaeng.area.repository.AreaRepository;
 import danpoong.mohaeng.course.domain.Course;
 import danpoong.mohaeng.course.domain.UserCourse;
+import danpoong.mohaeng.course.dto.AICourseRes;
 import danpoong.mohaeng.course.dto.CourseCreateReq;
 import danpoong.mohaeng.course.dto.CourseCreateRes;
 import danpoong.mohaeng.course.repository.CourseRepository;
@@ -11,6 +12,7 @@ import danpoong.mohaeng.course.repository.UserCourseRepository;
 import danpoong.mohaeng.disability.domain.UserDisability;
 import danpoong.mohaeng.disability.repository.DisabilityRepository;
 import danpoong.mohaeng.disability.repository.UserDisabilityRepository;
+import danpoong.mohaeng.location.domain.Location;
 import danpoong.mohaeng.location.repository.LocationRepository;
 import danpoong.mohaeng.user.domain.User;
 import danpoong.mohaeng.user.repository.UserRepository;
@@ -32,6 +34,7 @@ public class CourseService {
     private final DisabilityRepository disabilityRepository;
     private final UserDisabilityRepository userDisabilityRepository;
     private final CourseRepository courseRepository;
+    private final AIRecService aiRecService;
 
     public CourseCreateRes createTrip(CourseCreateReq courseCreateReq) {
         // 코스 정보 생성
@@ -113,5 +116,21 @@ public class CourseService {
             userCourseRepository.save(userCourse);
             log.info("코스 관광지 정보 : {}", userCourse.getLocation().getContentTitle());
         }
+    }
+
+    public AICourseRes createAIRecCourse(List<Long> disability, List<Long> tripType, Long area, Long period) {
+
+        // 필터링 된 관광지
+        List<Location> filteredLocation = locationRepository.filterByAreaAndDisabilityAndTravelType(
+                        area, disability, tripType).stream()
+                .limit(80)
+                .toList();
+
+        // 필터링 된 음식점
+        List<Location> filteredRestaurant = locationRepository.filterByAreaAndContentType(area).stream()
+                .limit(80)
+                .toList();
+
+        return aiRecService.generateCourse(filteredLocation, filteredRestaurant, areaRepository.findByNumber(area).getName(), period);
     }
 }
